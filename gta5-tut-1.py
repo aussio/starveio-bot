c14365500a1039102550d0d2a78dfdf248fa685e
aussio <austin.curtis@wpengine.com>
2017-06-23 18:10:43 -0500

    Following tutorial for gtav

--- gta5-tut-1.py
+++ gta5-tut-1.py
@@ -4,13 +4,14 @@ import cv2
 import time
 
 def screen_record():
+    box = (0, 194, 1200, 800)
     last_time = time.time()
     while(True):
         # 800x600 windowed mode
-        printscreen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        printscreen =  ImageGrab.grab(box)
         print('loop took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
-        cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        cv2.imshow('window',np.array(printscreen))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
