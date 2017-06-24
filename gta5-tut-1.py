c71a3d0281e3ad15b3c81b44dc84e60aaa336d23
aussio <austin.curtis@wpengine.com>
2017-06-23 20:10:27 -0500

    Dynamic monitor size and faster caps.

--- gta5-tut-1.py
+++ gta5-tut-1.py
@@ -1,47 +1,76 @@
 import numpy as np
-from PIL import ImageGrab
 import cv2
 import time
 
-TOP_LEFT_X = 0
-TOP_LEFT_Y = 100
-BOT_RIGHT_X = 720
-BOT_RIGHT_Y = 800
+from PIL import ImageGrab, Image
+from mss.darwin import MSS as mss
 
-WIDTH = BOT_RIGHT_X - TOP_LEFT_X
-HEIGHT = BOT_RIGHT_Y - TOP_LEFT_Y
+TOP_MAC_BAR = 21
+CHROME_EXTRAS = 97
+TOP_LEFT_Y = CHROME_EXTRAS - TOP_MAC_BAR
 
-def capture_screen(wait=True):
-    box = (TOP_LEFT_X * 2,
-           TOP_LEFT_Y * 2,
-           BOT_RIGHT_X * 2,
-           BOT_RIGHT_Y * 2
-           )
-    screen = np.array(ImageGrab.grab(box).convert('RGB'))
-    # convert color
-    b, g, r = cv2.split(screen)
-    screen = cv2.merge([r, g, b])
-    # setup window settings
-    window_title = "ZachWasHere"
-    cv2.namedWindow(window_title, cv2.WINDOW_NORMAL)
-    # resize window
-    cv2.resizeWindow(window_title, WIDTH, HEIGHT)
-    # resize image
-    screen = cv2.resize(screen, (WIDTH, HEIGHT))
-    # create window and display image
-    cv2.imshow(window_title, screen)
-    if wait:
-        cv2.waitKey()
+class ScreenCapture:
+    def __init__(self):
+        self.captured_area = {}
 
-def screen_record():
-    last_time = time.time()
-    while(True):
-        print('loop took {} seconds'.format(time.time()-last_time))
+    def get_image(self):
+        try:
+            with mss() as sct:
+                # We retrieve monitors informations:
+                monitors = sct.enum_display_monitors()
+
+                # Get rid of the first, as it represents the "All in One" monitor:
+                for num, monitor in enumerate(monitors[1:], 1):
+                    # Get raw pixels from the screen.
+                    # This method will store screen size into `width` and `height`
+                    # and raw pixels into `image`.
+                    # Don't capture chrome header
+                    monitor['top'] = monitor['top'] + TOP_LEFT_Y
+                    monitor['width'] = monitor['width'] // 2
+                    monitor['height'] = monitor['height'] - TOP_LEFT_Y
+                    # set self.monitor for sharing.
+                    self.captured_area = monitor
+                    sct.get_pixels(self.captured_area)
+
+                    # Create an Image
+                    size = (sct.width, sct.height)
+                    return Image.frombytes('RGB', size, sct.image)
+        except ScreenshotError as ex:
+            print(ex)
+
+    def capture_screen(self, wait=True):
+        screen = np.array(self.get_image())
+        # convert color
+        screen = cv2.cvtColor(screen, cv2.COLOR_RGB2BGR)
+        # setup window settings
+        window_title = "ZachWasHere"
+        cv2.namedWindow(window_title, cv2.WINDOW_NORMAL)
+        # resize window
+        cv2.resizeWindow(window_title,
+                         self.captured_area['width'],
+                         self.captured_area['height'])
+        # resize image
+        screen = cv2.resize(screen,
+                            (self.captured_area['width'],
+                             self.captured_area['height'])
+                           )
+
+        # create window and display image
+        cv2.imshow(window_title, screen)
+        if wait:
+            cv2.waitKey()
+
+    def screen_record(self):
         last_time = time.time()
-        capture_screen(wait=False)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
+        while(True):
+            print('loop took {} seconds'.format(time.time()-last_time))
+            last_time = time.time()
+            self.capture_screen(wait=False)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
 
-#capture_screen()
-screen_record()
+if __name__ == '__main__':
+    sc = ScreenCapture()
+    #sc.capture_screen()
+    sc.screen_record()
